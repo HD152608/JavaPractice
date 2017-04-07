a2ee96b0518b88273c61a3bc48ba0d933f8e4a36
덮어라.pc <.pc@DESKTOP-MB6A5SV>
2017-04-07 09:24:18 +0900

    실습과제6 과자배열

---
+++ JavaPractice/src/org/dimigo/oop/Snack.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *  |_ Snack
+ *
+ * 1. 개요: 과자 배열
+ * 2. 작성일: 2017. 4. 6.
+ * </pre>
+ *
+ * @author     : 덮어라.pc
+ * @version    : 1.0
+ */
+public class Snack {
+	private String name;
+	private String company;
+	private int price;
+	private int number;
+
+	/**
+	 * @param args
+	 */
+	public Snack()
+	{	
+	}
+	
+	public Snack(String name, String company, int price, int number)
+	{	
+		this.name= name;
+		this.company= company;
+		this.price= price;
+		this.number= number;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	public String getCompany(){
+		return company;
+	}
+	public int getPrice(){
+		return price;
+	}
+	public int getNumber(){
+		return number;
+	}
+	// setter 메소드 선언 
+	public void setName(String newName){
+		name = newName;
+	}
+	public void setCompany(String newCompany){
+		company = newCompany;
+	}
+	public void setPrice(int newPrice){
+		price = newPrice;
+	}
+	public void setNumber(int newNumber) {
+		number = newNumber;
+	}
+	
+	public int calcPrice()
+	{
+		return price * number;
+	}
+	public String toString()
+	{
+		return "이름 : " + name + "\n제조사 : " + company + "\n가격 : " + String.format("%,d", price) + "원\n개수 : " + number + "개\n";
+	}
+}

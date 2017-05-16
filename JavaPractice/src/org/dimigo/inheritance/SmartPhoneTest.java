d5f11e026abc831be18b751206b513536db64db6
덮어라.pc <.pc@DESKTOP-MB6A5SV>
2017-05-16 10:48:43 +0900

    실습과제11 스마트폰 경쟁

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *  |_ SmartPhoneTest
+ *
+ * 1. 개요: 스마트폰 경쟁
+ * 2. 작성일: 2017. 5. 16.
+ * </pre>
+ *
+ * @author     : 덮어라.pc
+ * @version    : 1.0
+ */
+public class SmartPhoneTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SmartPhone [] phones ={
+				new IPhone("iPhone 7", "애플", 900000),
+				new Galaxy("갤럭시 S8", "삼성", 800000)
+		};
+		for(SmartPhone v : phones){
+			System.out.println(v);
+			v.turnOn();
+			v.pay();
+			v.useSpecialFunction(v);
+			v.turnOff();
+			System.out.println();
+		}
+		
+
+	}
+
+}
